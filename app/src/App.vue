c031e5a7848159e57947bc20884b1b82970bd5cd
Adrián <adrianncatalan@gmail.com>
2024-02-16 23:00:51 +0100

    replacing everything to use tailwindcss

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,31 +1,16 @@
 <template>
-  <div class="navigaton-shared">
+  <div class="navigation-shared">
     <NavigationShared />
     <router-view></router-view>
-    <FooterShared />
   </div>
 </template>
 
 <script>
 import { defineAsyncComponent } from 'vue'
-import FooterShared from './modules/shared/components/FooterShared.vue';
 
 export default {
   components: {
     NavigationShared: defineAsyncComponent(() => import('@/modules/shared/components/NavigationShared.vue')),
-    FooterShared
   }
 }
 </script>
-
-<style lang="scss">
-body {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-.navigaton-shared {
-  width: 100%;
-}
-</style>
