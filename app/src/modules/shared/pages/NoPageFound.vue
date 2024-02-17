4c8449098e79123e0a7167d9d947b4e0ba72dbf4
Adrián <adrianncatalan@gmail.com>
2024-02-17 18:47:34 +0100

    more impletations

--- app/src/modules/shared/pages/NoPageFound.vue
+++ app/src/modules/shared/pages/NoPageFound.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="min-h-screen pt-16 pb-12 flex flex-col bg-white">
+    <div class="min-h-screen flex flex-col bg-white">
         <main class="flex-grow flex flex-col justify-center max-w-7xl w-full
         mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex-shrink-0 flex justify-center">
-                <h2 class="flex justify-center text-4xl text-indigo-600 opacity-70 font-bold mt-4">Cosmo Explorer you seem
+                <h2 class="flex justify-center text-4xl text-indigo-600 opacity-70 font-bold">Cosmo Explorer you seem
                     to have lost your way!</h2>
             </div>
             <div class="py-16">
