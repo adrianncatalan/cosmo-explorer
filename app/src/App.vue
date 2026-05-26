4c711e99d5de7b6970e7bbf1d58726a0c67824ab
Adrián Catalán <adrianncatalan@gmail.com>
2026-05-26 21:35:14 +0200

    fix: make footer stick to bottom on pages with little content
    - Wrap layout in min-h-screen flex flex-col container
    - Add flex-grow to main so footer is pushed to viewport bottom
    - Remove misleading 'navigation-shared' class
    
    Closes #2

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="navigation-shared">
-    <NavigationShared />
-    <router-view></router-view>
-    <FooterShared />
-  </div>
+    <div class="min-h-screen flex flex-col">
+        <NavigationShared />
+        <main class="flex-grow">
+            <router-view />
+        </main>
+        <FooterShared />
+    </div>
 </template>
 
 <script>
-import { defineAsyncComponent } from 'vue'
-import FooterShared from './modules/shared/components/FooterShared.vue';
+import { defineAsyncComponent } from 'vue';
+import FooterShared from '@/modules/shared/components/FooterShared.vue';
 
 export default {
-  components: {
-    NavigationShared: defineAsyncComponent(() => import('@/modules/shared/components/NavigationShared.vue')),
-    FooterShared
-  }
-}
+    components: {
+        NavigationShared: defineAsyncComponent(() => import('@/modules/shared/components/NavigationShared.vue')),
+        FooterShared
+    }
+};
 </script>
