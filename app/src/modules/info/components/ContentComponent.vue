676b235bc597cd76b9be07073b6cbec8e98a99cc
Adrián Catalán <adrianncatalan@gmail.com>
2026-05-26 21:24:54 +0200

    refactor: reorganize project into consistent module-based structure
    - Move nasaApiService to shared/services
    - Remove trivial ContentComponent and TitleComponent
    - Merge their content directly into HomePage
    - Rename 'info' module to 'home' for clarity
    - Update router import path accordingly
    
    Closes #1

--- app/src/modules/info/components/ContentComponent.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="content">
-        <p class="content-text">Welcome to the website! Here, you'll find access to a wide range of information about space
-            exploration, all
-            sourced directly from the NASA API.
-        </p>
-    </div>
-</template>
